3d1cac43f149c9dbbc8f1d38d9ae035ee8756d8a
Jonathan Wren <jonathan@nowandwren.com>
2017-11-02 15:54:05 -0700

    update init script a bit more

--- Scripts/install-dotfiles/init.sh
+++ Scripts/install-dotfiles/init.sh
@@ -16,5 +16,26 @@ if command -v git >/dev/null 2>&1; then
 else
   printf "Git isn't installed! Using other means to get repo into '${DOTFILES_DIR}'...\n"
   curl -L https://github.com/jonathanwren/dotfiles/archive/master.zip -o /tmp/dotfiles.zip
-  unzip /tmp/dotfiles.zip -d "$DOTFILES_PARENT" && mv "${DOTFILES_PARENT}/dotfiles-master" "$DOTFILES_DIR" && cd "$DOTFILES_DIR" && ./Scripts/init.sh && rm /tmp/dotfiles.zip
+  unzip /tmp/dotfiles.zip -d "$DOTFILES_PARENT"
+  mv "${DOTFILES_PARENT}/dotfiles-master" "$DOTFILES_DIR"
+
+  # Clean up
+  rm /tmp/dotfiles.zip
 fi
+
+# Should we do the thing?
+read -p "Do you want to install now, too? (y/n)? " answer
+case ${answer:0:1} in
+  y|Y|Yes|yes|YES )
+    # Start the install
+    cd "$DOTFILES_DIR"
+    ./Scripts/init.sh
+  ;;
+  * )
+    # @todo add more info about installing later here
+    printf '\nExiting...\n\nBy the way, you can finish the install later by running this command:\n  sh %s/Scripts/init.sh\n' "$DOTFILES_DIR"
+    printf '\nBye!\n'
+  ;;
+esac
+
+
