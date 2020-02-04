6fbcc2afdcbc782af058e980ccf3f02480629bff
Jonathan Wren <jonathan@nowandwren.com>
2020-02-04 11:33:46 -0800

    point at correct location for settings backup file

--- Scripts/mackup.sh
+++ Scripts/mackup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-cp -f "${DOTFILES_DIR}/Backup/.mackup.cfg" "${HOME}/"
-cp -rf "${DOTFILES_DIR}/Backup/.mackup" "${HOME}/"
+cp -f "${DOTFILES_DIR}/.mackup.cfg" "${HOME}/"
+cp -rf "${DOTFILES_DIR}/.mackup" "${HOME}/"
 
 mackup restore -f
+
