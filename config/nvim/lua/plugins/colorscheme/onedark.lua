1b589ea9743699af478a3eab73c8696a7c8fbc23
Jonathan Wren <jonathan@nowandwren.com>
2021-02-14 16:19:10 -0800

    coordinate tmux and nvim with respect to dimming active pane's background colors

--- config/nvim/lua/plugins/colorscheme/onedark.lua
+++ config/nvim/lua/plugins/colorscheme/onedark.lua
@@ -2,7 +2,7 @@
 --- Colorscheme overrides ---
 nvim_create_augroups({
   colorextend = {
-    'ColorScheme * call onedark#extend_highlight("Normal", { "bg": { "gui": "#1a1a1a" } })',
+    'ColorScheme * call onedark#extend_highlight("Normal", { "bg": { "gui": "NONE" } })',
     'ColorScheme * call onedark#set_highlight("NormalNC", { "bg": { "gui": "#212121", "cterm": 12 } })',
   }
 })
