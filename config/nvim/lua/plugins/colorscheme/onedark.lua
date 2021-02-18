abfeea827f7fc306ea8d11463690f952245d9e55
Jonathan Wren <jonathan@nowandwren.com>
2021-02-18 07:33:42 -0800

    adjust color schemes across apps to coordinate a bit better, make background slightly darker for better contrast

--- config/nvim/lua/plugins/colorscheme/onedark.lua
+++ config/nvim/lua/plugins/colorscheme/onedark.lua
@@ -4,5 +4,10 @@ nvim_create_augroups({
   colorextend = {
     'ColorScheme * call onedark#extend_highlight("Normal", { "bg": { "gui": "NONE" } })',
     'ColorScheme * call onedark#set_highlight("NormalNC", { "bg": { "gui": "#212121", "cterm": 12 } })',
+    'ColorScheme * call onedark#set_highlight("NormalFloat", { "bg": { "gui": "#1a1a1a", "cterm": 12 } })',
+    'ColorScheme * call onedark#set_highlight("PMenu", { "bg": { "gui": "#1a1a1a", "cterm": 12 } })',
+    'ColorScheme * call onedark#set_highlight("LspReferenceRead", { "bg": { "gui": "#626262", "cterm": 12 } })',
+    'ColorScheme * call onedark#set_highlight("LspReferenceText", { "bg": { "gui": "#626262", "cterm": 12 } })',
+    'ColorScheme * call onedark#set_highlight("LspReferenceWrite", { "bg": { "gui": "#626262", "cterm": 12 } })',
   }
 })
