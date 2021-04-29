72988e0785df1b7125ec961409387b3a06a71b0d
Jonathan Wren <jonathan@nowandwren.com>
2021-04-28 18:03:28 -0700

    Update git settings in git and nvim
    Add some aliases for git worktree
    Update gitgutter settings in neovim

--- config/nvim/lua/plugins/gitgutter.lua
+++ config/nvim/lua/plugins/gitgutter.lua
@@ -6,5 +6,8 @@ g.gitgutter_sign_added = ''
 g.gitgutter_sign_modified = ''
 g.gitgutter_sign_removed = ''
 g.gitgutter_sign_removed_first_line = '▔'
+g.gitgutter_sign_removed_above_and_below = '{'
 g.gitgutter_sign_modified_removed = ''
+g.gitgutter_sign_allow_clobber = 0
+
 -- g.gitgutter_set_sign_backgrounds = 1
