3ad9491ba395d39e63af942ab8997303b2775155
Jonathan Wren <jonathan@nowandwren.com>
2021-06-03 08:44:17 -0700

    slow down scroll in nvim (it was way too fast)

--- config/nvim/lua/plugins/comfortable-motion.lua
+++ config/nvim/lua/plugins/comfortable-motion.lua
@@ -1,4 +1,4 @@
 -- Keymap --
 opts = { silent = true }
-map('n', '<C-d>', '<cmd>call comfortable_motion#flick(g:comfortable_motion_impulse_multiplier * winheight(0) * 2)<CR>', opts)
-map('n', '<C-u>', '<cmd>call comfortable_motion#flick(g:comfortable_motion_impulse_multiplier * winheight(0) * -2)<CR>', opts)
+map('n', '<C-d>', '<cmd>call comfortable_motion#flick(g:comfortable_motion_impulse_multiplier * winheight(0) * 0.5)<CR>', opts)
+map('n', '<C-u>', '<cmd>call comfortable_motion#flick(g:comfortable_motion_impulse_multiplier * winheight(0) * -0.5)<CR>', opts)
