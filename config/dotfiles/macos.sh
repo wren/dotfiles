29d43583a552c054cc2d66f500d5795e8fe1932e
Jonathan Wren <jonathan@nowandwren.com>
2021-08-04 20:16:55 -0700

    change key repeat setting in default macos setup

--- config/dotfiles/macos.sh
+++ config/dotfiles/macos.sh
@@ -201,8 +201,8 @@ defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
 # Follow the keyboard focus while zoomed in
 defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
 
-# Disable press-and-hold for keys in favor of key repeat
-# defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+# Disable press-and-hold for keys in favor of key repeat (because srsly)
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
 # defaults write NSGlobalDomain KeyRepeat -int 0
