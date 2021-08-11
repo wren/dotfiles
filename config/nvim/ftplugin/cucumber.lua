bb064d913ffd48f54d99631ac3b569c6f29201b0
Jonathan Wren <jonathan@nowandwren.com>
2021-08-10 22:49:23 -0700

    update ftplugin nvim files to lua now that v0.5 is out

---
+++ config/nvim/ftplugin/cucumber.lua
@@ -0,0 +1,9 @@
+local o = vim.opt_local
+o.foldmethod = indent
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.textwidth = 88
+o.expandtab = true
+o.smarttab = true
+o.smartindent = false
---
+++ config/nvim/ftplugin/yaml.lua
@@ -0,0 +1,5 @@
+local o = vim.opt_local
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.textwidth = 88
