fbdb0c4d3dbfe20a342fbb3eb30b724861cda13a
Jonathan Wren <jonathan@nowandwren.com>
2021-09-29 11:54:14 -0700

    disable some default plugins that we don't use

--- config/nvim/lua/core/defaults.lua
+++ config/nvim/lua/core/defaults.lua
@@ -8,6 +8,9 @@ CACHE_PATH = fn.fnamemodify(fn.stdpath('cache'), ':h')
 DATA_PATH = fn.stdpath('data')
 
 -- Disable default vim plugins --
+
+-- let g:loaded_man = 1
+g.loaded_2html_plugin = 1
 g.loaded_getscript = 1
 g.loaded_getscriptPlugin = 1
 g.loaded_gzip = 1
@@ -15,17 +18,20 @@ g.loaded_logiPat = 1
 g.loaded_matchit = 1
 g.loaded_matchparen = 1
 g.netrw_nogx = 1 -- disable netrw's gx mapping.
+g.loaded_netrw = 1
 g.loaded_netrwPlugin = 1
+g.loaded_remote_plugins = 1
 g.loaded_rrhelper = 1
 g.loaded_shada_plugin = 1
 g.loaded_tar = 1
+g.loaded_spellfile_plugin = 1
 g.loaded_tarPlugin = 1
 g.loaded_tutor_mode_plugin = 1
-g.loaded_2html_plugin = 1
 g.loaded_vimball = 1
 g.loaded_vimballPlugin = 1
 g.loaded_zip = 1
 g.loaded_zipPlugin = 1
+
 
 -- Get rid of some default mappings --
 map('n', '<space>', '')
