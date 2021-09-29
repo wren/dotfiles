f4d3828ddb6ba267cfe0e052f31895de278f6d07
Jonathan Wren <jonathan@nowandwren.com>
2021-09-29 15:32:45 -0700

    update material theme config to new style

--- config/nvim/lua/plugins/material.lua
+++ config/nvim/lua/plugins/material.lua
@@ -1,52 +1,63 @@
--- Config
-g.material_style = "darker"
-g.material_italic_comments = true
-g.material_italic_keywords = true
-g.material_italic_functions = true
-g.material_italic_variables = false
-g.material_contrast = true
-g.material_borders = false
--- g.material_disable_background = true -- never enable this
-
-g.material_custom_colors = {
-  bg       = "#121212",
-  bg_alt  = "#0F0F0F",
+-- Reuse the themes colors when possible
+local material = require('material.colors')
+local custom = {
+  bg = '#141414',
+  bg_inactive = '#212121',
+  chrome = '#323232',
 }
 
--- Small wrapper to make overrides look nicer
-local function hi(name, my_colors)
-  color_pairs = {}
-  for key, color in pairs(my_colors) do
-    table.insert(color_pairs, string.format('%s=%s', key, color))
-  end
-  return string.format('ColorScheme * highlight %s %s', name, table.concat(color_pairs, ' '))
-end
-
-nvim_create_augroup('colorextend', {
-  hi('SignColumnNC', { guibg = '#212121' }),
-  hi('NormalNC',      { guibg = '#212121' }),
-  hi('IndentlineOne', { guifg = '#2D3439' }),
-  hi('IndentlineTwo', { guifg = '#4F5B64' }),
+-- Config
+g.material_style = 'darker'
+require('material').setup({
+  contrast = true,
+  borders = false,
+  contrast_windows = {
+    "terminal",
+    "packer",
+    "qf",
+    "nvim-tree",
+  },
+  text_contrast = {
+    lighter = false,
+    darker = true,
+  },
+  disable = {
+    background = false,
+    term_colors = false,
+    eob_lines = true,
+  },
+  custom_highlights = {
+    IndentlineOne     = { fg = '#2D3439', bg = nil },
+    IndentlineTwo     = { fg = '#4F5B64', bg = nil },
+    Normal            = { bg = custom.bg },
+    NormalNC          = { bg = custom.bg_inactive },
+    SignColumn        = { bg = nil },
+    -- SignColumnNC      = { bg = custom.bg_inactive },
+    VertSplit         = { fg = custom.chrome , bg = custom.chrome },
+    SignifySignAdd    = { fg = material.green, bg = nil },
+    SignifySignDelete = { fg = material.red, bg   = nil },
+    SignifySignChange = { fg = material.blue, bg  = nil },
+    SignifySignChangeDelete = { fg = material.orange, bg  = nil },
 
   -- Barbar styling (see https://github.com/romgrk/barbar.nvim#options)
-  -- hi('BufferCurrent',        { guifg = '#ff0000', guibg = '#00ff00' }),
-  -- hi('BufferCurrentIndex',   { guifg = '#ff0000', guibg = '#00ff00' }),
-  -- hi('BufferCurrentMod',     { guifg = '#ff0000', guibg = '#00ff00' }),
-  -- hi('BufferCurrentSign',    { guifg = '#ff0000', guibg = '#00ff00' }),
-  -- hi('BufferCurrentTarget',  { guifg = '#ff0000', guibg = '#00ff00' }),
-  -- hi('BufferVisible',        { guifg = '#ff0000', guibg = '#00ff00' }),
-  -- hi('BufferVisibleIndex',   { guifg = '#ff0000', guibg = '#00ff00' }),
-  -- hi('BufferVisibleMod',     { guifg = '#ff0000', guibg = '#00ff00' }),
-  -- hi('BufferVisibleSign',    { guifg = '#ff0000', guibg = '#00ff00' }),
-  -- hi('BufferVisibleTarget',  { guifg = '#ff0000', guibg = '#00ff00' }),
-  -- hi('BufferInactive',       { guifg = '#ff0000', guibg = '#00ff00' }),
-  -- hi('BufferInactiveIndex',  { guifg = '#ff0000', guibg = '#00ff00' }),
-  -- hi('BufferInactiveMod',    { guifg = '#ff0000', guibg = '#00ff00' }),
-  -- hi('BufferInactiveSign',   { guifg = '#ff0000', guibg = '#00ff00' }),
-  -- hi('BufferInactiveTarget', { guifg = '#ff0000', guibg = '#00ff00' }),
-  -- hi('BufferTabpages',       { guifg = '#ff0000', guibg = '#00ff00' }),
-  -- hi('BufferTabpageFill',    { guifg = '#ff0000', guibg = '#00ff00' }),
+    -- BufferCurrent =        { fg = '#ff0000', bg = '#00ff00' },
+    -- BufferCurrentIndex =   { fg = '#ff0000', bg = '#00ff00' },
+    -- BufferCurrentMod =     { fg = '#ff0000', bg = '#00ff00' },
+    -- BufferCurrentSign =    { fg = '#ff0000', bg = '#00ff00' },
+    -- BufferCurrentTarget =  { fg = '#ff0000', bg = '#00ff00' },
+    -- BufferVisible =        { fg = '#ff0000', bg = '#00ff00' },
+    -- BufferVisibleIndex =   { fg = '#ff0000', bg = '#00ff00' },
+    -- BufferVisibleMod =     { fg = '#ff0000', bg = '#00ff00' },
+    -- BufferVisibleSign =    { fg = '#ff0000', bg = '#00ff00' },
+    -- BufferVisibleTarget =  { fg = '#ff0000', bg = '#00ff00' },
+    -- BufferInactive =       { fg = '#ff0000', bg = '#00ff00' },
+    -- BufferInactiveIndex =  { fg = '#ff0000', bg = '#00ff00' },
+    -- BufferInactiveMod =    { fg = '#ff0000', bg = '#00ff00' },
+    -- BufferInactiveSign =   { fg = '#ff0000', bg = '#00ff00' },
+    -- BufferInactiveTarget = { fg = '#ff0000', bg = '#00ff00' },
+    -- BufferTabpages =       { fg = '#ff0000', bg = '#00ff00' },
+    -- BufferTabpageFill =    { fg = '#ff0000', bg = '#00ff00' },
+  },
 })
 
--- require'material'.set()
 cmd 'colorscheme material'
