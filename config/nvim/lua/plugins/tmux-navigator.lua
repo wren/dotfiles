f984d16389f6699de7a4b324d9cea1cc927d6fe8
Jonathan Wren <jonathan@nowandwren.com>
2021-10-20 15:42:25 -0700

    fix tmux/nvim navigator keybindings

--- config/nvim/lua/plugins/tmux-navigator.lua
+++ config/nvim/lua/plugins/tmux-navigator.lua
@@ -1,8 +1,8 @@
 g.tmux_navigator_no_mappings = 1
 -- Keymap --
 opts = { silent = true }
-map('n', '<a-h>', ':TmuxNavigateLeft<cr>', opts)
-map('n', '<a-j>', ':TmuxNavigateDown<cr>', opts)
-map('n', '<a-k>', ':TmuxNavigateUp<cr>', opts)
-map('n', '<a-l>', ':TmuxNavigateRight<cr>', opts)
-map('n', '<a-o>', ':TmuxNavigatePrevious<cr>', opts)
+map('n', '<M-h>', ':TmuxNavigateLeft<cr>', opts)
+map('n', '<M-j>', ':TmuxNavigateDown<cr>', opts)
+map('n', '<M-k>', ':TmuxNavigateUp<cr>', opts)
+map('n', '<M-l>', ':TmuxNavigateRight<cr>', opts)
+map('n', '<M-o>', ':TmuxNavigatePrevious<cr>', opts)
