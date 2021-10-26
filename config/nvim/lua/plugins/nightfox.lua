80e8a0268c34edbf7b103d6c4d98a36a10e8edcc
Jonathan Wren <jonathan@nowandwren.com>
2021-10-26 10:36:53 -0700

    update color scheme settings for nvim

--- config/nvim/lua/plugins/nightfox.lua
+++ config/nvim/lua/plugins/nightfox.lua
@@ -1,41 +1,48 @@
 local nightfox = require('nightfox')
 local custom = {
   bg = '#141414',
-  bg_inactive = '#1C1C1C',
+  bg_inactive = '#212121',
   chrome = '#323232',
 }
 
 nightfox.setup({
-  fox = "nightfox",
+  fox = 'nightfox',
   transparent = false,
-  -- alt_nc = custom.bg_inactive,
+  alt_nc = custom.bg_inactive,
   styles = {
     -- Style that is applied to variables: see `highlight-args` for options
-    comments = "NONE",
-    functions = "NONE",
-    keywords = "NONE",
-    strings = "NONE",
-    variables = "NONE",
+    comments = 'NONE',
+    functions = 'NONE',
+    keywords = 'NONE',
+    strings = 'NONE',
+    variables = 'NONE',
   },
   inverse = {
     match_paren = false, -- Enable/Disable inverse highlighting for match parens
     visual = false, -- Enable/Disable inverse highlighting for visual selection
     search = false, -- Enable/Disable inverse highlights for search highlights
   },
-  colors = {},
+  colors = {
+    -- To see defaults run:
+    --   :Bufferize lua print(vim.inspect(require('nightfox.colors').load()))`
+    bg = '#121212',
+    bg_alt = '#212121',
+    bg_sidebar = '#212121',
+
+  },
   hlgroups = {
     -- ExampleClass = { fg = '#fff', bg = '#fff', style = 'bold, sp = 'underline' },
-    IndentlineOne     = { fg = '#2D3439', bg = "NONE" },
-    IndentlineTwo     = { fg = '#4F5B64', bg = "NONE" },
-    -- Normal            = { bg = custom.bg },
+    IndentlineOne     = { fg = '#2D3439', bg = 'NONE' },
+    IndentlineTwo     = { fg = '#4F5B64', bg = 'NONE' },
+    Normal            = { bg = 'NONE' },
     -- NormalNC          = { bg = custom.bg_inactive },
-    SignColumn        = { bg = "NONE" },
+    SignColumn        = { bg = 'NONE' },
     -- SignColumnNC      = { bg = custom.bg_inactive },
     -- VertSplit         = { fg = custom.chrome , bg = custom.chrome },
-    SignifySignAdd    = { fg = "${green}", bg = "NONE" },
-    SignifySignDelete = { fg = "${red}", bg   = "NONE" },
-    SignifySignChange = { fg = "${blue}", bg  = "NONE" },
-    SignifySignChangeDelete = { fg = "${orange}", bg  = "NONE" },
+    -- SignifySignAdd    = { fg = '${green}', bg = 'NONE' },
+    -- SignifySignDelete = { fg = '${red}', bg   = 'NONE' },
+    -- SignifySignChange = { fg = '${blue}', bg  = 'NONE' },
+    -- SignifySignChangeDelete = { fg = '${orange}', bg  = 'NONE' },
   }
 })
 
