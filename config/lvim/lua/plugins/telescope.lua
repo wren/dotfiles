2983433f105561096af29fdaa346e854a3ed25f3
Jonathan Wren <jonathan@nowandwren.com>
2021-12-27 11:53:24 -0800

    use hook in telescope config

--- config/lvim/lua/plugins/telescope.lua
+++ config/lvim/lua/plugins/telescope.lua
@@ -24,20 +24,22 @@ plugin.defaults.layout_config = {
 -- add some mappings, but don't erase the defaults
 plugin.defaults.mappings.i['<C-/>'] = 'which_key'
 
--- Keymap --
-local silent = { silent = true }
-map('n', '<leader>fb', ':Telescope buffers<CR>', silent)
-map('n', '<leader>ft', ':Telescope filetypes<CR>', silent)
-map('n', '<M-C-p>', ':Telescope buffers<CR>', silent)
-map('n', '<C-p>', ':Telescope find_files<CR>', silent)
-map('n', '<leader>fp', ':Telescope find_files<CR>')
-map('n', '<leader>ff', ':Telescope live_grep<CR>')
-map('n', '<leader>fm', ':Telescope keymaps<CR>')
-map('n', '<leader>fk', ':Telescope help_tags<CR>')
-map('n', '<leader>fc', ':Telescope commands<CR>')
-map('n', '<leader>hc', ':Telescope cocmmand_history<CR>')
-map('n', '<leader>hs', ':Telescope search_history<CR>')
-map('n', '<leader>hf', ':Telescope oldfiles<CR>')
-map('n', '<leader>fs', ':Telescope colorscheme<CR>', silent)
-map('n',  '<leader>fw', ':Telescope grep_string<CR>', silent)
-map('n',  'z=', ':Telescope spell_suggest theme=cursor<CR>', silent)
+plugin.on_config_done = function()
+  -- Keymap --
+  local silent = { silent = true }
+  map('n', '<leader>fb', ':Telescope buffers<CR>', silent)
+  map('n', '<leader>ft', ':Telescope filetypes<CR>', silent)
+  map('n', '<M-C-p>', ':Telescope buffers<CR>', silent)
+  map('n', '<C-p>', ':Telescope find_files<CR>', silent)
+  map('n', '<leader>fp', ':Telescope find_files<CR>')
+  map('n', '<leader>ff', ':Telescope live_grep<CR>')
+  map('n', '<leader>fm', ':Telescope keymaps<CR>')
+  map('n', '<leader>fk', ':Telescope help_tags<CR>')
+  map('n', '<leader>fc', ':Telescope commands<CR>')
+  map('n', '<leader>hc', ':Telescope cocmmand_history<CR>')
+  map('n', '<leader>hs', ':Telescope search_history<CR>')
+  map('n', '<leader>hf', ':Telescope oldfiles<CR>')
+  map('n', '<leader>fs', ':Telescope colorscheme<CR>', silent)
+  map('n',  '<leader>fw', ':Telescope grep_string<CR>', silent)
+  map('n',  'z=', ':Telescope spell_suggest theme=cursor<CR>', silent)
+end
