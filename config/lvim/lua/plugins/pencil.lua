83e6c36fb482e7056a17cd283434c76272dbd69a
Jonathan Wren <jonathan@nowandwren.com>
2022-01-11 11:02:43 -0800

    update config for pencil nvim plugin

--- config/lvim/lua/plugins/pencil.lua
+++ config/lvim/lua/plugins/pencil.lua
@@ -5,3 +5,9 @@ local plugin = { 'preservim/vim-pencil' }
 plugin.ft = { 'jrnl' }
 
 table.insert(lvim.plugins, plugin)
+
+
+-- options
+-- 0=disable, 1=one char, 2=hide char, 3=hide all (def)
+g['pencil#conceallevel'] = 2
+
