042700abd28f44c29875f6f3d9e5988858b0892f
Jonathan Wren <jonathan@nowandwren.com>
2022-01-18 11:35:36 -0800

    update nvim autocommands to use lvim

--- config/neovim/lua/core/events.lua
+++ config/neovim/lua/core/events.lua
@@ -1,32 +1,29 @@
-
-nvim_create_augroups({
+lvim.autocommands.custom_groups = {
 
   -- Disable swap/undo/viminfo/shada files in temp directories or shm
-  disable_temp_meta = {
-    {
-      'BufNewFile,BufReadPre',
-      '/tmp/*,$TMPDIR/*,$TMP/*,$TEMP/*,*/shm/*,/private/var/*,*.noindex/*',
-      'setlocal noswapfile noundofile nobackup nowritebackup viminfo= shada='
-    }
+  {
+    'BufNewFile,BufReadPre',
+    '/tmp/*,$TMPDIR/*,$TMP/*,$TEMP/*,*/shm/*,/private/var/*,*.noindex/*',
+    'setlocal noswapfile noundofile nobackup nowritebackup viminfo= shada='
   },
 
   -- better autocomplete for commands
-  dynamic_smartcase = {
+  {
     'CmdLineEnter : set nosmartcase',
     'CmdLineLeave : set smartcase'
   },
 
   -- change directory when a dir (not a file) is opened
-  cd_if_directory = { 'BufEnter * lua cd_if_open_directory()' },
+  { 'BufEnter * lua cd_if_open_directory()' },
 
   -- turn off cursorline when window isn't active
-  cursorline_on_active = {
+  {
     'WinEnter,InsertLeave * set cursorline',
     'WinLeave,InsertEnter * set nocursorline'
   },
 
   -- put the cursor back on the last know position when opening a file
-  last_known_cursor_position = {
+  {
     [[ BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g'\"" | endif ]]
   },
 
@@ -35,12 +32,7 @@ nvim_create_augroups({
   --   'BufReadPost,BufModifiedSet * lua if opt.modifiable:get() then keymap_modifiable_only() end'
   -- },
 
-  -- better syntax highlight performance with large files
-  -- syntax_many_lines = {
-  --   [[Syntax * if 5000 < line('$') | syntax sync minlines=200 | endif]]
-  -- },
-
-  delete_empty_buffers = {
+  {
     'BufWinLeave * silent lua delete_unused_buffer()'
   },
-})
+}
