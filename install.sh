c9d783ac0266bbc920e2a76ea7d265814bd02d1b
Jonathan Wren <jonathan@nowandwren.com>
2022-01-26 10:25:49 -0800

    move install to install.sh, standardize on install.conf.yaml for config files

--- install.sh
+++ install.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env zsh
 set -e
+# Sets up and runs dotbot + plugins
 
 # Config Options
 DOTFILES_REPO="https://github.com/wren/dotfiles.git"
 DOTBOT_REPO="https://github.com/wren/dotfiles-dotbot.git"
 
 DOTFILES_DIR="${0:A:h}"
-DOTBOT_CONFIG="$DOTFILES_DIR/config/dotfiles/dotbot.yaml"
+DOTBOT_CONFIG="$DOTFILES_DIR/install.conf.yaml"
 DOTBOT_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/dotbot"
 DOTBOT_PLUGINS_DIR="$DOTBOT_DIR/plugins"
 DOTBOT_BIN="$DOTBOT_DIR/dotbot/bin/dotbot"
 
+mkdir -p $DOTBOT_DIR
+cd $DOTBOT_DIR
+
 # Check dotbot directory
 if ! git -C $DOTBOT_DIR rev-parse; then
-  mkdir -p $DOTBOT_DIR
   git clone $DOTBOT_REPO $DOTBOT_DIR
 fi
 
 # Make sure dotbot is same version across machines
-cd $DOTBOT_DIR
 git pull --force origin main
 git submodule update --init --recursive
 
@@ -30,6 +32,7 @@ if ! git -C $DOTFILES_DIR rev-parse; then
   mkdir -p $DOTFILES_DIR
   git clone $DOTFILES_REPO $DOTFILES_DIR
 fi
+
 cd $DOTFILES_DIR
 
 # Load all plugins, and run dotbot
@@ -37,16 +40,10 @@ CMD=$DOTBOT_BIN
 for dir in $DOTBOT_PLUGINS_DIR/*; do
   CMD+=" --plugin-dir=$dir"
 done
+# CMD+=' --verbose'
 
-# Run global dotbot config first
+export DOTBOT_CMD=$CMD
+
+# Run dotbot
 print -P "%F{006}───── Dotbot ─────%f"
 ${=CMD} -d $DOTFILES_DIR -c $DOTBOT_CONFIG "$@" || true
-
-# Then run any individual ones from various directories
-set +e # if any steps fail, then the remaining steps should still happen
-for file in $(find . -name 'install.conf.yaml'); do
-  shortname="${file%%/install.conf.yaml}"
-  shortname="${shortname##./config/}"
-  print -P "\n\n%F{006}───── ${(U)shortname} ─────%f"
-  ${=CMD} -d $(dirname $file) -c $file "$@"
-done
