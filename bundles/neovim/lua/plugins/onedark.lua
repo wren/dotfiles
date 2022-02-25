3dc310ada8672f985c3997e41d73977ad6c02978
Jonathan Wren <jonathan@nowandwren.com>
2022-02-25 11:03:26 -0800

    update neovim styling to dim inactive windows

--- bundles/neovim/lua/plugins/onedark.lua
+++ bundles/neovim/lua/plugins/onedark.lua
@@ -5,14 +5,15 @@ plugin.config = function()
   local od = require('onedark')
   od.setup {
     style = 'darker',
-    ending_tildes = true,
+    ending_tildes = false,
     colors = {
       -- dim_grey = '#2d3035',
     },
     highlights = {
       -- ExampleNC = {fg = '#0000ff', bg = '#00ff00', sp = '$cyan', fmt = 'underline,italic'},
-      -- NormalNC = { bg = '#2d3035' },
-      -- SignColumnNC = { bg = '#2d3035' },
+      NormalNC = { bg = '#2d3035' },
+      EndOfBuffer = { bg = 'NONE'},
+      SignColumn = { bg = 'NONE' },
     }
   }
   od.load()
