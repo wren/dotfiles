3489a3ad8e8c5e1ad770bf242c0746d748415fc6
Jonathan Wren <jonathan@nowandwren.com>
2022-03-04 22:16:13 -0800

    clean up some old env vars

--- install.sh
+++ install.sh
@@ -3,15 +3,11 @@ set -e
 # Sets up and runs dotbot + plugins
 
 # Config Options
-DOTFILES_REPO="https://github.com/wren/dotfiles.git"
+DOTFILES_DIR="${DOTFILES_DIR:-$HOME/Dotfiles}"
 DOTBOT_REPO="https://github.com/wren/dotfiles-dotbot.git"
-
-DOTFILES_DIR="${0:A:h}"
 DOTBOT_CONFIG="$DOTFILES_DIR/install.conf.yaml"
 DOTBOT_DEFAULTS="$DOTFILES_DIR/defaults.conf.yaml"
 DOTBOT_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/dotbot"
-DOTBOT_PLUGINS_DIR="$DOTBOT_DIR/plugins"
-DOTBOT_BIN="$DOTBOT_DIR/dotbot/bin/dotbot"
 
 # Check dotbot directory
 mkdir -p $DOTBOT_DIR
@@ -28,8 +24,8 @@ export DOTBOT_CONFIG
 export DOTBOT_DEFAULTS
 
 # Load all plugins, and run dotbot
-CMD=$DOTBOT_BIN
-for dir in $DOTBOT_PLUGINS_DIR/*; do
+CMD=$DOTBOT_DIR/dotbot/bin/dotbot
+for dir in $DOTBOT_PLUGINS_DIR/plugins/*; do
   CMD+=" --plugin-dir=$dir"
 done
 # CMD+=' --verbose'
