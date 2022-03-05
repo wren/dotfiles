2f9efbee5b80fc63c2b02200a3f3dca29d1854fc
Jonathan Wren <jonathan@nowandwren.com>
2022-03-04 22:13:17 -0800

    make install script more useful on first run

--- install.sh
+++ install.sh
@@ -13,30 +13,19 @@ DOTBOT_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/dotbot"
 DOTBOT_PLUGINS_DIR="$DOTBOT_DIR/plugins"
 DOTBOT_BIN="$DOTBOT_DIR/dotbot/bin/dotbot"
 
+# Check dotbot directory
 mkdir -p $DOTBOT_DIR
 cd $DOTBOT_DIR
-
-# Check dotbot directory
 if ! git -C $DOTBOT_DIR rev-parse; then
   git clone $DOTBOT_REPO $DOTBOT_DIR
 fi
-
-# Make sure dotbot is same version across machines
 git pull --force origin main
 git submodule update --init --recursive
 
-# Prep dotfiles dir
+# Prep env vars
 export DOTFILES_DIR
 export DOTBOT_CONFIG
 export DOTBOT_DEFAULTS
-
-# Check dotfiles directory
-if ! git -C $DOTFILES_DIR rev-parse; then
-  mkdir -p $DOTFILES_DIR
-  git clone $DOTFILES_REPO $DOTFILES_DIR
-fi
-
-cd $DOTFILES_DIR
 
 # Load all plugins, and run dotbot
 CMD=$DOTBOT_BIN
