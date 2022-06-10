297a33e9d6dffa52c4b1f2f253e681a72033314e
Jonathan Wren <jonathan@nowandwren.com>
2022-06-10 11:47:41 -0700

    Update autocommands per LunarVim/LunarVim#2592

--- bundles/neovim/lua/core/events.lua
+++ bundles/neovim/lua/core/events.lua
@@ -1,41 +1,94 @@
-lvim.autocommands.custom_groups = {
+lvim.autocommands = {
 
   -- Disable swap/undo/viminfo/shada files in temp directories or shm
   {
-    'BufNewFile,BufReadPre',
-    '/tmp/*,$TMPDIR/*,$TMP/*,$TEMP/*,*/shm/*,/private/var/*,*.noindex/*',
-    'setlocal noswapfile noundofile nobackup nowritebackup viminfo= shada='
+    {'BufNewFile', 'BufReadPre'},
+    {
+      pattern = {
+        '/tmp/*',
+        '$TMPDIR/*',
+        '$TMP/*',
+        '$TEMP/*',
+        '*/shm/*',
+        '/private/var/*',
+        '*.noindex/*'
+      },
+      command = 'setlocal noswapfile noundofile nobackup nowritebackup viminfo= shada='
+    }
   },
 
   -- better autocomplete for commands
-  { 'CmdLineEnter', ':', 'set nosmartcase' },
-  { 'CmdLineLeave', ':', 'set smartcase' },
+  {
+    'CmdLineEnter',
+    {
+      pattern = ':',
+      command = 'set nosmartcase'
+    }
+  },
+  {
+    'CmdLineLeave',
+    {
+      pattern = ':',
+      command = 'set smartcase'
+    }
+  },
 
   -- change directory when a dir (not a file) is opened
-  { 'BufEnter', '*', 'lua cd_if_open_directory()' },
+  -- { 'BufEnter', pattern = {'*'}, command = 'lua cd_if_open_directory()' },
+  {
+    'BufEnter',
+    {
+      pattern = '*',
+      callback = _G.cd_if_open_directory
+    }
+  },
 
   -- turn off cursorline when window isn't active
   -- see: https://vim.fandom.com/wiki/Highlight_current_line
-  { 'VimEnter,WinEnter,BufWinEnter', '*', 'setlocal cursorline'},
-  { 'WinLeave', '*', 'setlocal nocursorline'  },
-
-  -- put the cursor back on the last know position when opening a file
   {
-    'BufReadPost',
-    '*',
-    [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g'\"" | endif ]]
+    {'VimEnter', 'WinEnter', 'BufWinEnter'},
+    {
+      pattern = '*',
+      command = 'setlocal cursorline'
+    }
+  },
+  {
+    'WinLeave',
+    {
+      pattern = '*',
+      command = 'setlocal nocursorline'
+    },
   },
 
   -- Delete empty buffers when tabbing away from them
-  { 'BufWinLeave', '*', 'silent lua delete_unused_buffer()' },
+  -- { 'BufWinLeave', {pattern = {'*'}, command = 'silent lua delete_unused_buffer()' }},
+  {
+    'BufWinLeave',
+    {
+      pattern = '*',
+      callback = _G.delete_unused_buffer
+    }
+  },
 
   -- set keymap for bindings that don't work on unmodifiable buffers (e.g. help)
   {
-    'BufReadPost,BufModifiedSet',
-    '*',
-    'lua if opt.modifiable:get() then map_buffer_group(_G.MODIFIABLE_ONLY_BINDINGS) end'
+    {'BufReadPost', 'BufModifiedSet'},
+    {
+      pattern = '*',
+      callback = function()
+        if opt.modifiable:get() then
+          map_buffer_group(_G.MODIFIABLE_ONLY_BINDINGS)
+        end
+      end
+    }
   },
 
   -- get out of command mode by pressing <esc> in normal mode
-  { 'CmdwinEnter', '*', 'map <buffer> <esc> <c-c><c-c>' },
+  {
+    'CmdwinEnter',
+    {
+      pattern = '*',
+      command = 'map <buffer> <esc> <c-c><c-c>'
+    }
+  }
 }
