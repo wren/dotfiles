c48e15e288bc7f08248da7d251f47dbdaa1e09a2
Jonathan Wren <jonathan@nowandwren.com>
2022-10-23 17:03:22 -0700

    update interesting words config in neovim

--- bundles/neovim/lua/plugins/interestingwords.lua
+++ bundles/neovim/lua/plugins/interestingwords.lua
@@ -12,28 +12,25 @@ plugin.config = function()
   g.interestingWordsDefaultMappings = 0
   g.interestingWordsGUIColors = { '#8CCBEA', '#A4E57E', '#FFDB72', '#FF7272', '#FFB3FF', '#9999FF' }
   g.interestingWordsRandomiseColors = 1
-
-  -- Keymap --
-  map('n', '<localleader>*',     ':call InterestingWords("n")<cr>', {silent = true})
-  map('v', '<localleader>*',     ':call InterestingWords("v")<cr>', {silent = true})
-  map('n', '<localleader><c-L>', ':call UncolorAllWords()<cr>',     {silent = true})
-  map('n', '<localleader>n',     ':call WordNavigation(1)<cr>',     {silent = true})
-  map('n', '<localleader>N',     ':call WordNavigation(0)<cr>',     {silent = true})
-
-  -- Which key --
-  -- @todo update this to the lunar style
-  local status, wk = pcall(require, 'which-key')
-  if(status) then
-    wk.register({
-      ["<localleader>"] = {
-        ['*'] = { 'Highlight <word>' },
-        ['<c-L'] = { 'Clear highlighted words' },
-        n = { 'Next highlighted word' },
-        N = { 'Previous highlighted word' },
-      }
-    })
-  end
 end
 
-table.insert(lvim.plugins, plugin)
+-- Keymap --
+local opts = { silent = true }
+map('n', '<localleader>w*',     ':call InterestingWords("n")<cr>', opts)
+map('v', '<localleader>w*',     ':call InterestingWords("v")<cr>', opts)
+map('n', '<localleader>w<c-l>', ':call UncolorAllWords()<cr>',     opts)
+map('n', '<localleader>wn',     ':call WordNavigation(1)<cr>',     opts)
+map('n', '<localleader>wN',     ':call WordNavigation(0)<cr>',     opts)
 
+-- Which key --
+which_key_register_if_loaded({
+  ["<localleader>w"] = {
+    name = "Interesting Words",
+    ['*'] = { 'Highlight <word>' },
+    ['<c-l>'] = { 'Clear highlighted words' },
+    n = { 'Next highlighted word' },
+    N = { 'Previous highlighted word' },
+  }
+})
+
+table.insert(lvim.plugins, plugin)
