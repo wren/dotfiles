f5b5a858d56386a044f44872c7bb6390d1b9276e
Jonathan Wren <jonathan@nowandwren.com>
2022-10-23 17:02:40 -0700

    update debug python config in neovim

--- bundles/neovim/lua/plugins/dap.lua
+++ bundles/neovim/lua/plugins/dap.lua
@@ -1,13 +1,14 @@
 -- Debugging module for various languages
 -- Individual language plugins installed below
-
 lvim.builtin.dap.active = true
 
 -- Python --
 local plugin = { 'mfussenegger/nvim-dap-python' }
 
 plugin.config = function()
-  require('dap-python').setup(PYTHON_VENV .. '/python')
+  local dap = require('dap-python')
+  dap.setup(PYTHON_VENV .. '/python')
+  dap.test_runner = 'pytest'
 end
 
 table.insert(lvim.plugins, plugin)
