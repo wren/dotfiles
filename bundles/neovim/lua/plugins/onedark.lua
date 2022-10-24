61b9ab0ce11dc4e604bd6d1c767bff410cc14e83
Jonathan Wren <jonathan@nowandwren.com>
2022-10-23 17:05:47 -0700

    update color scheme in neovim

--- bundles/neovim/lua/plugins/onedark.lua
+++ bundles/neovim/lua/plugins/onedark.lua
@@ -7,15 +7,19 @@ plugin.config = function()
     style = 'darker',
     ending_tildes = false,
     colors = {
-      -- dim_grey = '#2d3035',
+      dark_grey = '#282C34',
+      dim_grey = '#606570',
+      mid_grey = '#4C515D',
     },
     highlights = {
       -- ExampleNC = {fg = '#0000ff', bg = '#00ff00', sp = '$cyan', fmt = 'underline,italic'},
-      NormalNC = { bg = '#2d3035' },
+      NormalNC = { bg = '$dark_grey' },
       EndOfBuffer = { bg = 'NONE'},
       SignColumn = { bg = 'NONE' },
-      VertSplit = { bg = '#282C34' },
-      StatusLine = { bg = '#282C34' },
+      VertSplit = { bg = '$dark_grey' },
+      StatusLine = { bg = '$dark_grey' },
+      IndentlineOne = { fg = '$mid_grey' },
+      IndentlineTwo = { fg = '$dim_grey' },
     }
   }
   od.load()
