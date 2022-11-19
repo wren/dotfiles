7174e02640496c2be2c1a07bb142d05f02b5a7af
Jonathan Wren <jonathan@nowandwren.com>
2022-11-19 10:56:29 -0800

    update sync script for first install on linux

--- sync.sh
+++ sync.sh
@@ -1,11 +1,16 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 set -e
 # Sets up and runs ansible
 
 # Config Options
 export DOTFILES_DIR="${DOTFILES_DIR:-$HOME/Dotfiles}"
 
+PATH=~/.local/bin:$PATH
+
 if ! command -v ansible >/dev/null; then
+  if ! command -v pip >/dev/null; then
+    sudo apt install python3-pip -y
+  fi
   python3 -m pip install ansible
 fi
 
