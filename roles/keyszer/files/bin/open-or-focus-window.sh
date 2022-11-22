440b0a083f7adef89f62cd9c8281710b6a8f9166
Jonathan Wren <jonathan@nowandwren.com>
2022-11-22 13:37:26 -0800

    add script to open/focus an app (for global shortcuts)

---
+++ roles/keyszer/files/bin/open-or-focus-window.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+wanted_id="$(xdotool search --limit=1 --classname "$1")"
+current_id="$(xdotool getwindowfocus)"
+
+if [[ -z "$wanted_id" ]]; then
+  runuser -u "$2" -- "/bin/$1" &
+  exit 0
+fi
+
+if [[ "$current_id" == "$wanted_id" ]]; then
+  xdotool windowminimize "$wanted_id"
+else
+  xdotool windowactivate "$wanted_id"
+fi
