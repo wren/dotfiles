161363df4f9544b5c3dcfa189e6462b099edc353
Jonathan Wren <jonathan@nowandwren.com>
2022-11-23 10:45:19 -0800

    lolcat wasn't working without some extra path shenanigans

--- roles/zsh/files/config/path.sh
+++ roles/zsh/files/config/path.sh
@@ -9,6 +9,9 @@ path=(
   # Version/package manager(s) near the front of the line
   $HOME/.local/opt/asdf-vm/bin
 
+  # apt installs some stuff here for some reason
+  /usr/games
+
   # Homebrew because ugh
   /home/linuxbrew/.linuxbrew/bin
 
