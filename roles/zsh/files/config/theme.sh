c74168a84060ebb72022600dc45f5a79f3756ab9
Jonathan Wren <jonathan@nowandwren.com>
2022-11-25 10:35:46 -0800

    add sudo.with.env alias and change prompt color when root

--- roles/zsh/files/config/theme.sh
+++ roles/zsh/files/config/theme.sh
@@ -411,7 +411,7 @@
 
   ##################################[ context: user@hostname ]##################################
   # Context color when running with privileges.
-  typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=13
+  typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=9
   # Context color in SSH without privileges.
   typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_FOREGROUND=180
   # Default context color (no privileges, no SSH).
