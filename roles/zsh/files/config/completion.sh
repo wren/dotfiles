942090b8697111a992dc81838d6213da8af53a26
Jonathan Wren <jonathan@nowandwren.com>
2022-11-27 10:57:07 -0800

    quick cleanup and whitespace changes

--- roles/zsh/files/config/completion.sh
+++ roles/zsh/files/config/completion.sh
@@ -1,18 +1,5 @@
 # --- MISC COMPLETIONS --- #
 
-# @TODO: Fix these for zsh
-# # Add tab completion for SSH hostnames based on ~/.ssh/config, ignoring wildcards
-# [ -e "$HOME/.ssh/config" ] && \
-# complete -o "default" -o "nospace" -W "$(grep "^[Hh]ost" ~/.ssh/configs/* | grep -v "[?*]" | cut -d " " -f2 | tr ' ' '\n')" \
-# scp sftp ssh rsync ssh.hostinfo ssh.clearknownhost ssh.hostname
-#
-# # Add tab completion for `defaults read|write NSGlobalDomain`
-# # You could just use `-g` instead, but I like being explicit
-# complete -W "NSGlobalDomain" defaults
-#
-# # Add `killall` tab completion for common apps
-# complete -o "nospace" -W "Contacts Calendar Dock Finder Mail Safari iTunes SystemUIServer Terminal Twitter" killall
-
 # pipx
 { register-python-argcomplete && eval "$(register-python-argcomplete pipx)" } &>/dev/null
 
@@ -28,8 +15,8 @@ zstyle ':completion:*' cache-path "$XDG_CACHE_PATH/zsh"
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'
 
 # Fast Completion Cache
-# Adapted from the various iterations at this url:
-# https://gist.github.com/ctechols/ca1035271ad134841284
+# Adapted from the various iterations here:
+# @see: https://gist.github.com/ctechols/ca1035271ad134841284
 () {
   setopt local_options
   setopt extendedglob
