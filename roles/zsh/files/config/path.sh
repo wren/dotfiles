eac7a3091f5f87c7250cf3192765013d65900702
Jonathan Wren <jonathan@nowandwren.com>
2022-11-27 14:48:59 -0800

    add asdf version manager and python (uses asdf) to ansible

--- roles/zsh/files/config/path.sh
+++ roles/zsh/files/config/path.sh
@@ -6,23 +6,18 @@ path=(
   # our local shenanigans get priority
   $HOME/.local/bin
 
-  # Version/package manager(s) near the front of the line
-  $HOME/.local/opt/asdf-vm/bin
-
   # apt installs some stuff here for some reason
   /usr/games
 
-  # Homebrew because ugh
-  /home/linuxbrew/.linuxbrew/bin
-
   # default path
   $path
+
+  # Homebrew because ugh
+  /home/linuxbrew/.linuxbrew/bin
 )
 
 fpath=(
   $ZDOTDIR/functions
-
-  ${ASDF_DIR}/completions
 
   #default fpath
   $fpath
