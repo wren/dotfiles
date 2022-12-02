d3fe8d8b82b70818d209f7029fa0361bed6e703e
Jonathan Wren <jonathan@nowandwren.com>
2022-12-02 11:27:11 -0800

    add 'ansible-beauatiful-output' callback to make output nicer looking

--- sync.sh
+++ sync.sh
@@ -9,9 +9,10 @@ PATH=~/.local/bin:$PATH
 
 if ! command -v ansible >/dev/null; then
   if ! command -v pip >/dev/null; then
+    # mac ships with pip, so we only install on linux
     sudo apt install python3-pip -y
   fi
-  python3 -m pip install ansible
+  python3 -m pip install ansible simplejson
 fi
 
 ansible-galaxy install -r requirements.yaml
