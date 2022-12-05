ff6224c70bed1d0045c24341f2ecb6965aca6149
Jonathan Wren <jonathan@nowandwren.com>
2022-12-04 17:12:33 -0800

    add headers to sync file

--- sync.sh
+++ sync.sh
@@ -7,6 +7,10 @@ export DOTFILES_DIR="${DOTFILES_DIR:-$HOME/Dotfiles}"
 
 PATH=~/.local/bin:$PATH
 
+function print-header() {
+  printf -- "\n$(tput setaf 6)───── %s ─────$(tput sgr0)\n" "$@"
+}
+
 if ! command -v ansible >/dev/null; then
   if ! command -v pip >/dev/null; then
     # mac ships with pip, so we only install on linux
@@ -15,5 +19,8 @@ if ! command -v ansible >/dev/null; then
   python3 -m pip install ansible simplejson
 fi
 
+print-header 'Pre-reqs'
 ansible-galaxy install -r requirements.yaml
+
+print-header 'Tasks'
 ansible-playbook "${DOTFILES_DIR}/dotfiles.yaml" --ask-become-pass -i hosts "$@"
