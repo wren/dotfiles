861ff8313f2a6732a059a4e5180295705b77422b
Jonathan Wren <jonathan@nowandwren.com>
2023-05-20 11:44:41 -0700

    update firefox ansible role

---
+++ roles/firefox/files/user.js
@@ -0,0 +1,14 @@
+// This file contains settings for Firefox
+// see: http://kb.mozillazine.org/User.js_file
+// see: http://kb.mozillazine.org/About:config_entries
+
+// Use the userChrome.css file in the profile directory
+// https://www.userchrome.org/how-create-userchrome-css.html#aboutconfig
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", "true");
+
+// Put the sidebar on the right
+// https://discourse.mozilla.org/t/open-sidebar-to-the-right-by-default-possible/48005/2
+user_pref("sidebar.position_start", "false");
+
+// Highlight all in find
+user_pref("findbar.highlightAll", "true");
