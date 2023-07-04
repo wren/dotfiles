07e441e8ba52baf04f0622866e1f62173706ece7
Jonathan Wren <jonathan@nowandwren.com>
2023-07-04 00:36:22 -0700

    update neovim options module

--- roles/neovim/files/config/config/options.lua
+++ roles/neovim/files/config/config/options.lua
@@ -1,20 +1,9 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-local opt = vim.opt
-
--- folding
-function _G.custom_fold_text()
-  local line = vim.fn.getline(vim.v.foldstart)
-  local line_count = string.format("%s lines", vim.v.foldend - vim.v.foldstart + 1)
-  local divider = string.rep(" ", fn.winwidth(0) - #line - #line_count - 2)
-  return string.format("%s%s %s", line, divider, line_count)
-end
-
+-- Defaults: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 opt.foldenable = false
 opt.foldlevelstart = 99
 opt.foldtext = "v:lua.custom_fold_text()"
-
+opt.whichwrap:append("<,>,h,l")
+opt.wrap = false
 opt.listchars:append({
   nbsp = "␠",
   extends = "→",
@@ -22,3 +11,20 @@ opt.listchars:append({
   tab = "├─",
   trail = "·",
 })
+opt.scrolloff = 2
+opt.sidescrolloff = 10
+
+-- Secure sensitive information, disable backup files in temp directories
+opt.secure = true
+if fn.exists("&backupskip") ~= 0 then
+  opt.backupskip:append({
+    "/tmp/*",
+    "$TMPDIR/*",
+    "$TMP/*",
+    "$TEMP/*",
+    "*/shm/*",
+    "/private/var/*",
+    "*.nobackup/*",
+    "*.noindex/*",
+  })
+end
