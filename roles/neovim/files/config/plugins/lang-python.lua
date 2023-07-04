79537bf3a81855956e6aeeca5fbb97ed22d3ca40
Jonathan Wren <jonathan@nowandwren.com>
2023-07-04 00:40:02 -0700

    add starter config for python in neovim

---
+++ roles/neovim/files/config/plugins/lang-python.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "python",
+      })
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "pyright",
+        "ruff",
+      })
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {},
+      },
+    },
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      table.insert(opts.sources, nls.builtins.formatting.ruff)
+      table.insert(opts.sources, nls.builtins.diagnostics.ruff)
+    end,
+  },
+
+  {
+    -- TODO: Enable dap support for python
+    -- "mfussenegger/nvim-dap",
+    -- optional = true,
+    -- dependencies = {
+    --   "suketa/nvim-dap-ruby",
+    --   config = function()
+    --     require("dap-ruby").setup()
+    --   end,
+    -- },
+  },
+}
