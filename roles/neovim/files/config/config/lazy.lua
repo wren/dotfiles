182b98b864e68faf488b22f826cb4da417d7ab7e
Jonathan Wren <jonathan@nowandwren.com>
2023-07-04 01:14:47 -0700

    clean up some comments

--- roles/neovim/files/config/config/lazy.lua
+++ roles/neovim/files/config/config/lazy.lua
@@ -12,10 +12,8 @@ require("lazy").setup({
     { import = "plugins" },
   },
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-    lazy = false,
-    version = false, -- always use the latest git commit
+    lazy = true,
+    version = false,
   },
   checker = {
     -- automatically check for plugin updates
