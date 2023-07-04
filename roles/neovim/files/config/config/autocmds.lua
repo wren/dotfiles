089b53dc9a9b071b925dca6d6d3675b3afc33005
Jonathan Wren <jonathan@nowandwren.com>
2023-07-04 00:28:45 -0700

    add autocmd to get out of ex mode with esc

--- roles/neovim/files/config/config/autocmds.lua
+++ roles/neovim/files/config/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local create_autocmd = vim.api.nvim_create_autocmd
+
+-- get out of command mode by pressing <esc> in normal mode
+create_autocmd("CmdwinEnter", {
+  pattern = "*",
+  command = "map <buffer> <esc> <c-c><c-c>",
+})
