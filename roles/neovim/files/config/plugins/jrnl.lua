4dfc54c569ea16d9683e28b419ab5bb4dc745cc1
Jonathan Wren <jonathan@nowandwren.com>
2023-11-05 20:43:07 -0800

    replace abandoned plugin true-zen with zen-mode

--- roles/neovim/files/config/plugins/jrnl.lua
+++ roles/neovim/files/config/plugins/jrnl.lua
@@ -18,57 +18,24 @@ return {
     end,
   },
   {
-    -- nice formatting when writing (removes a lot of noise)
-    "Pocco81/TrueZen.nvim",
-
-    cmd = {
-      "TZAtaraxis",
-      "TZFocus",
-      "TZMinimalist",
-    },
-
+    "folke/zen-mode.nvim",
     opts = {
-      modes = {
-        ataraxis = {
-          shade = "dark",
-          backdrop = 0, -- percentage by which padding windows should be dimmed/brightened. Must be a number between 0 and 1. Set to 0 to keep the same background color
-          minimum_writing_area = { -- minimum size of main window
-            width = 88,
-            height = 999,
-          },
-          options = { -- options to be disabled when entering Minimalist mode
-            number = false,
-            relativenumber = false,
-            showtabline = 0,
-            signcolumn = "no",
-            statusline = "",
-            cmdheight = 1,
-            laststatus = 0,
-            showcmd = false,
-            showmode = false,
-            ruler = false,
-            numberwidth = 1,
-          },
-          quit_untoggles = false,
-          padding = {
-            left = 52,
-            right = 52,
-            top = 10,
-            bottom = 10,
-          },
-          callbacks = {
-            open_pre = function()
-              local normal = vim.api.nvim_get_hl_by_name("Normal", true)
-              vim.api.nvim_set_hl(0, "NormalNC", { bg = normal.background })
-            end,
-            open_pos = nil,
-            close_pre = nil,
-            close_pos = nil,
-          },
+      window = {
+        backdrop = 1,
+        options = {
+          signcolumn = "no", -- disable signcolumn
+          number = false, -- disable number column
+          relativenumber = false, -- disable relative numbers
+          cursorline = false, -- disable cursorline
+          cursorcolumn = false, -- disable cursor column
+          foldcolumn = "0", -- disable fold column
+          list = false, -- disable whitespace characters
         },
       },
-      integrations = {
-        lualine = true,
+      plugins = {
+        options = {
+          laststatus = 0, -- turn off the statusline
+        },
       },
     },
   },
