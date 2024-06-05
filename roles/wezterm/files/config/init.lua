d8b6570e86279cbc936df2630d7aa4a765ddcc60
Jonathan Wren <jonathan@nowandwren.com>
2024-06-05 09:20:32 -0700

    add update options for wezterm

--- roles/wezterm/files/config/init.lua
+++ roles/wezterm/files/config/init.lua
@@ -6,6 +6,7 @@ merge(config, require("config.colors"))
 merge(config, require("config.window"))
 merge(config, require("config.mux"))
 merge(config, require("config.keymap"))
+merge(config, require("config.update"))
 merge(config, require("config.mouse"))
 
 return config
---
+++ roles/wezterm/files/config/update.lua
@@ -0,0 +1,6 @@
+local config = {}
+
+config.check_for_updates = true
+config.check_for_updates_interval_seconds = 86400
+
+return config
