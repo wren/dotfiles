78ff4b7b0f9eaff10f38ca791699070105f7eda5
Jonathan Wren <jonathan@nowandwren.com>
2024-08-16 11:05:06 -0700

    add some extra colors to markdown highlights in neovim

--- roles/neovim/files/config/core/functions.lua
+++ roles/neovim/files/config/core/functions.lua
@@ -1,25 +1,25 @@
-
 -- Misc functions (like maybe for autocommands or whatever)
 function _G.cd_if_open_directory()
-  local full_path = fn.expand('%:p')
+  local full_path = fn.expand("%:p")
   if fn.isdirectory(full_path) == 1 then
-    cmd(string.format('cd %s', fn.fnameescape(full_path)))
+    cmd(string.format("cd %s", fn.fnameescape(full_path)))
   end
 end
 
 -- delete an unnamed, unmodified, empty buffer (including directory buffers)
 function _G.delete_unused_buffer()
-  if (fn.bufname() == "" and opt.buftype:get() == "")
+  if
+    (fn.bufname() == "" and opt.buftype:get() == "")
     and not opt.modified:get()
-    and fn.line('$') == 1
-    and fn.getline(1) == ''
+    and fn.line("$") == 1
+    and fn.getline(1) == ""
   then
     local my_bufnr = fn.bufnr()
     -- this needs to have a slight delay or we'll get
     -- an error about the buffer still being in use
     vim.defer_fn(function()
       if api.nvim_buf_is_loaded(my_bufnr) then
-        cmd(string.format('bdelete %s', my_bufnr))
+        cmd(string.format("bdelete %s", my_bufnr))
       end
     end, 10)
   end
@@ -27,18 +27,18 @@ end
 
 -- show some useful info about highlight groups (for debugging color schemes)
 function _G.highlight_group()
-  local ids = fn.synstack(fn.line('.'), fn.col('.'))
-  local result = 'no group found'
+  local ids = fn.synstack(fn.line("."), fn.col("."))
+  local result = "no group found"
   for i, id in ipairs(ids) do
-    local current = fn.synIDattr(id, 'name')
-    local linked = fn.synIDattr(fn.synIDtrans(id), 'name')
-    local icon = ''
+    local current = fn.synIDattr(id, "name")
+    local linked = fn.synIDattr(fn.synIDtrans(id), "name")
+    local icon = ""
     if i == #ids then
-      icon = ''
+      icon = ""
     end
-    result = string.format('%s %s', icon, current)
+    result = string.format("%s %s", icon, current)
     if current ~= linked then
-      result = string.format('%s  %s', result, linked)
+      result = string.format("%s  %s", result, linked)
     end
   end
   print(result)
@@ -46,21 +46,21 @@ end
 
 function _G.get_visual_selection()
   vim.cmd('noau normal! "vy"')
-  local text = vim.fn.getreg('v')
-  vim.fn.setreg('v', {})
+  local text = vim.fn.getreg("v")
+  vim.fn.setreg("v", {})
 
   text = string.gsub(text, "\n", "")
   if #text > 0 then
     return text
   else
-    return ''
+    return ""
   end
 end
 
 function _G.custom_fold_text()
   local line = vim.fn.getline(vim.v.foldstart)
   local line_count = string.format("%s lines", vim.v.foldend - vim.v.foldstart + 1)
-  local divider = string.rep(' ', fn.winwidth(0) - #line - #line_count - 2)
+  local divider = string.rep(" ", fn.winwidth(0) - #line - #line_count - 2)
   return string.format("%s%s %s", line, divider, line_count)
 end
 
@@ -71,14 +71,14 @@ function _G.nav_wezterm_split(key)
 
   if current ~= next then
     -- keep in nvim
-    cmd('wincmd '..key)
+    cmd("wincmd " .. key)
   else
     -- forward to wezterm
     local key_directions = {
-      h = 'Left',
-      j = 'Down',
-      k = 'Up',
-      l = 'Right',
+      h = "Left",
+      j = "Down",
+      k = "Up",
+      l = "Right",
     }
     local cmd = "wezterm cli activate-pane-direction " .. key_directions[key]
     local handle = io.popen(cmd)
--- roles/neovim/files/config/plugins/color.lua
+++ roles/neovim/files/config/plugins/color.lua
@@ -11,7 +11,18 @@ return {
       colors = {
         dark = {
           bg = "#202328",
+          bg_darker = "require('onedarkpro.helpers').darken('#202328', 2)",
+          bg_darkest = "require('onedarkpro.helpers').darken('bg', 8, 'onedark')",
         },
+      },
+      highlights = {
+        ["@markup.heading.1.markdown"] = { fg = "${orange}", bold = true },
+        ["@markup.heading.2.markdown"] = { fg = "${purple}", bold = true },
+        ["@markup.heading.3.markdown"] = { fg = "${green}", bold = false },
+        ["@markup.heading.4.markdown"] = { fg = "${blue}", bold = false },
+        ["@markup.heading.5.markdown"] = { fg = "${yellow}", bold = false },
+        ["@markup.heading.6.markdown"] = { fg = "${gray}", bold = false },
+        ["@markup.raw.block.markdown"] = { bg = "${bg_darker}" },
       },
     },
   },
