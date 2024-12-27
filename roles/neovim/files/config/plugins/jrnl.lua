d644fec097721f216067d3956f0c1d33808cd82e
Jonathan Wren <jonathan@nowandwren.com>
2024-12-26 17:54:26 -0800

    add some markdown formatting and functionality for jrnl files in neovim

--- roles/neovim/files/config/plugins/jrnl.lua
+++ roles/neovim/files/config/plugins/jrnl.lua
@@ -5,6 +5,79 @@ return {
     branch = "rework-structure-5",
   },
   {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "jrnl" },
+    opts = {
+      file_types = { "jrnl" },
+      bullet = {
+        icons = { "" },
+      },
+      heading = {
+        sign = false,
+        icons = {
+          "█▐ ",
+          "█▐▐ ",
+          "█▐▐▐ ",
+          "█▐▐▐▐ ",
+          "█▐▐▐▐▐ ",
+          "█▐▐▐▐▐▐ ",
+        },
+        -- border = true,
+        -- border_prefix = true,
+        -- border_virtual = false,
+        -- above = "▁",
+        -- below = "▔",
+      },
+      checkbox = {
+        enabled = true,
+        position = "inline",
+        unchecked = {
+          icon = "",
+          highlight = "RenderMarkdownUnchecked",
+          scope_highlight = "RenderMarkdownUncheckedText",
+        },
+        checked = {
+          icon = "",
+          highlight = "RenderMarkdownChecked",
+          scope_highlight = "RenderMarkdownCheckedText",
+        },
+        custom = {
+          todo = { raw = "[-]", rendered = "󰥔", highlight = "RenderMarkdownTodo", scope_highlight = nil },
+          info = {
+            raw = "[i]",
+            rendered = "",
+            highlight = "RenderMarkdownTodoInfoIcon",
+            scope_highlight = "RenderMarkdownTodoInfoText",
+          },
+          question = {
+            raw = "[?]",
+            rendered = "",
+            highlight = "RenderMarkdownTodoQuestionIcon",
+            scope_highlight = "RenderMarkdownTodoQuestionText",
+          },
+          active = {
+            raw = "[.]",
+            rendered = "",
+            highlight = "RenderMarkdownTodoActiveIcon",
+            scope_highlight = "RenderMarkdownTodoActiveText",
+          },
+          important = {
+            raw = "[!]",
+            rendered = " ",
+            highlight = "RenderMarkdownTodoImportantIcon",
+            scope_highlight = "RenderMarkdownTodoImportantText",
+          },
+          star = {
+            raw = "[*]",
+            rendered = "",
+            highlight = "RenderMarkdownTodoStarIcon",
+            scope_highlight = "RenderMarkdownTodoStarText",
+          },
+        },
+      },
+    },
+  },
+  {
     -- Changes some settings to make writing prose/notes
     -- easier and nicer (ass opposed to writing code)
     "preservim/vim-pencil",
